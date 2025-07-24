ca448dd2ede9c6cc15da0572aa6b00a8ea8ae4f7
Pedro Furmam <132955344+pedrofurmam@users.noreply.github.com>
2025-07-24 15:09:49 -0300

    ...

--- desenvolvimentoweb/webFinal-20240511T235101Z-001/webFinal/irLista.js
+++
@@ -1,7 +0,0 @@
-var criar_button = document.getElementById("criarLista");
-
-criar_button.addEventListener("click", (e) => {
-    e.preventDefault();
-    location.href = 'criarLista.html'
-
-});
--- desenvolvimentoweb/webFinal-20240511T235101Z-001/webFinal/logout.js
+++
@@ -1,9 +0,0 @@
-var logout_form = document.getElementById("logout-form");
-var submit_form = document.getElementById("logout");
-
-submit_form.addEventListener("click", (e) => {
-    e.preventDefault();
-    localStorage.removeItem("usuarioLogado")
-    window.location.href = 'index.html';
-
-})
